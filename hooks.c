fb26d78d2b96ad2b5ca84221e32afe2440999d84
misratasci <misratasci@gmail.com>
2024-04-24 15:58:29 +0300

    zoom in yapabiliyorum ama tam ortaya

--- hooks.c
+++ hooks.c
@@ -6,15 +6,15 @@
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:16:47 by mitasci           #+#    #+#             */
-/*   Updated: 2024/04/24 15:28:22 by mitasci          ###   ########.fr       */
+/*   Updated: 2024/04/24 15:58:07 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-void	zoom_in(double *scale, double zoom_speed)
+void	zoom_in(t_app *app, double zoom_speed)
 {
-	*scale += zoom_speed;
+	app->scale += zoom_speed;
 }
 
 int	handle_mousewheel(int button, int x, int y, t_app *app)
@@ -24,7 +24,8 @@ int	handle_mousewheel(int button, int x, int y, t_app *app)
         printf("Mouse wheel up\n");
 		printf("%f\n", app->scale);
 		printf("x: %d, y: %d\n", x, y);
-		fill_image(app->win_size, &(app->img), app->scale + 10.0);
+		zoom_in(app, 20.0);
+		fill_image(app->win_size, &(app->img), app->scale);
 		mlx_put_image_to_window(app->mlx, app->mlx_win, app->img.img, 0, 0);
     }
     else if (button == 5)
