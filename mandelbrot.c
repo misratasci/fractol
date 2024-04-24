09a1aee30c86efaa4895e381d13d3343fd1afc5a
misratasci <misratasci@gmail.com>
2024-04-24 18:46:04 +0300

    a

--- mandelbrot.c
+++ mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:10:19 by mitasci           #+#    #+#             */
-/*   Updated: 2024/04/24 17:26:02 by mitasci          ###   ########.fr       */
+/*   Updated: 2024/04/24 18:33:08 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int	get_mandelbrot_pixel(int x, int y, t_app app)
 
 	c[0] = scale_x(x, app.scale, app.offset_x);
 	c[1] = scale_y(y, app.scale, app.offset_y);
+	
 	z[0] = 0;
 	z[1] = 0;
 	z2[0] = 0;
@@ -39,4 +40,45 @@ int	get_mandelbrot_pixel(int x, int y, t_app app)
 		i++;
 	}
 	return (get_color(i, max_iter));
+}
+
+double	rev_scale_x(int c1, double scale, int offset)
+{
+	return (c1 * scale + offset);
+}
+
+double	rev_scale_y(int c2, double scale, int offset)
+{
+	return ((-1) * (c2 * scale + offset));
+}
+
+unsigned int	get_prev_pixel(double c1, double c2, t_app app)
+{
+	int	x;
+	int	y;
+
+	x = rev_scale_x(c1, app.scale, app.offset_x);
+	y = rev_scale_y(c2, app.scale, app.offset_y);
+	return (get_prev_color(app.img, x, y));
+}
+
+unsigned int rgb_to_hex(int r, int g, int b) {
+    return (r << 16) | (g << 8) | b;
+}
+
+unsigned int	get_prev_color(t_data img, int x, int y)
+{
+	int	pix;
+	int	color;
+
+	pix = (x * (img.bits_per_pixel / 8)) + (y * img.line_length);
+	color = rgb_to_hex(img.addr[pix], img.addr[pix + 1], img.addr[pix + 2]);
+	return (color);
+}
+
+int	*get_prev_cx(t_app app)
+{
+	int	*prev_cx;
+
+	prev_cx = (int *)malloc(sizeof(int) * )
 }
